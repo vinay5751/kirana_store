04217ab46c8f7681a26abf0b4a064a95b111e5e3
vinay5751 <vinay.sharma5751880@gmail.com>
2025-06-12 12:24:37 +0530

    feat(inventory): register product and category models in admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Product
+
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+    search_fields = ("name", "description")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "selling_price", "quantity")
+    list_filter = ("category",)
+    search_fields = ("name", "category")
