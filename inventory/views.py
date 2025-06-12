1ea7319f89901bc7578e47e5c886cd43f560f4f1
vinay5751 <vinay.sharma5751880@gmail.com>
2025-06-12 12:57:29 +0530

    feat(inventory): create product list page

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Product
+
 
 # Create your views here.
+def product_list(request):
+    products = Product.objects.all().order_by("name")
+
+    context = {"products": products}
+
+    return render(request, "inventory/product_list.html", context)
